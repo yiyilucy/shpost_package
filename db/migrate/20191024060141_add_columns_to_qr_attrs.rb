dc201d2a987e747f4141f1461faa4d62be075460
yiyilucy <1258349806@qq.com>
2019-10-24 14:31:40 +0800

    add columns to qr_attr

--- db/migrate/20191024060141_add_columns_to_qr_attrs.rb
+++ db/migrate/20191024060141_add_columns_to_qr_attrs.rb
@@ -1,7 +1,6 @@
 class AddColumnsToQrAttrs < ActiveRecord::Migration
   def change
   	add_column :qr_attrs, :id_num, :string
-  	add_column :qr_attrs, :receive_date, :datetime
   	add_column :qr_attrs, :province, :string
   	add_column :qr_attrs, :city, :string
   	add_column :qr_attrs, :district, :string
